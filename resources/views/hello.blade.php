6ffcec53e4f5347baf7ac300721cb520caa2d054
Pogiwogi2024 <paulanthonyparaan@gmail.com>
2025-04-10 11:24:40 +0800

    Initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<x-layouts.app :title="__('Hello World')">  
+
+<div class="relative mb-6 w-full">
+    <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Techtaxi.inc') }}</flux:subheading>
+    <flux:separator variant="subtle" />
+
+<p class="italic ...">Paul anthony paraan ...</p>
+
+</div>
+
+</x-layouts.app>
